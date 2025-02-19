49fbf9879fbf87fd468fb272a2b94864cfdb2230
Radu <radumost2001@gmail.com>
2025-02-19 10:55:38 +0200

    Add the code for driver manager and TestRunner fist launch

--- src/main/java/com/opencart/TestRunner.java
+++ src/main/java/com/opencart/TestRunner.java
@@ -1,7 +1,26 @@
 package com.opencart;
 
+import com.opencart.managers.DriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
+
 public class TestRunner {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = DriverManager.getInstance().getDriver();
+        driver.get("https://www.google.com/");
+
+        String firstTabName = driver.getWindowHandle();
+
+        //Open new window and navigate to Diez.md
+        driver.switchTo().newWindow(WindowType.TAB);
+        driver.get("https://www.diez.md/");
+        Thread.sleep(2000);
+        driver.close();
+        //Close ccurent Tab
+
+        driver.switchTo().window(firstTabName);
+        driver.get("https://www.stiri.md/");
+        Thread.sleep(2000);
+        driver.quit();
     }
 }
--- src/main/java/com/opencart/managers/DriverManager.java
+++ src/main/java/com/opencart/managers/DriverManager.java
@@ -1,4 +1,46 @@
 package com.opencart.managers;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
 public class DriverManager {
+    private static DriverManager instance;
+    private WebDriver driver;
+    private static final String WEB_DRIVER_TYPE = "Chrome";
+
+    private DriverManager(){
+        switch(WEB_DRIVER_TYPE.toUpperCase()) {
+            case "CHROME":
+                driver = new ChromeDriver();
+                System.out.println("Chrome driver loaded");
+                break;
+            case "FIREFOX":
+                driver = new FirefoxDriver();
+                System.out.println("Firefox driver loaded");
+                break;
+            case "EDGE":
+                driver = new EdgeDriver();
+                System.out.println("Edge driver loaded");
+                break;
+            case "SAFARI":
+                driver = new SafariDriver();
+                System.out.println("Safari driver loaded");
+                break;
+            default:
+                System.out.println("Invalid Driver Type");
+        }
+    }
+    public static DriverManager getInstance(){
+        if(instance == null){
+            instance = new DriverManager();
+        }
+            return instance;
+    }
+
+    public WebDriver getDriver(){
+        return driver;
+    }
 }
